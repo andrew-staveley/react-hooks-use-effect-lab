62ee934ffff39e533dc9f6730cef48e6cc72b05e
Andrew <andrewstaveley14@gmail.com>
2023-12-02 20:06:36 -0500

    Completed Assignmnet

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    //Starts Timer
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1);
+    }, 1000);
+
+    //Calls onAnswered when timer hits 0
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    };
+
+    //Cleanup function
+    return function cleanUp() {
+      clearTimeout(timer);
+    };
+
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
